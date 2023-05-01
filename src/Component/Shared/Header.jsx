6b5f4ad6d0d710ec8ae272826494410d88d91a3b
Farukul Islam <farukgb1999@gmail.com>
2023-05-02 02:34:12 +0600

    navbar and footer design

---
+++ src/Component/Shared/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import NavigationBar from "./Navigationbar";
+
+const Header = () => {
+  return (
+    <div>
+    <NavigationBar></NavigationBar>
+      <div className="hero mt-3 w-full min-h-screen bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">Hello there</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import Header from '../Component/Shared/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Component/Shared/Footer';
 
 const Main = () => {
     return (
-        <div>
-            
+        <div className=''>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
